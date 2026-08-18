8b265aa0f9cf7871a0b6c2c082bee9220bed429c
vfgkakarot-blip <vfgkakarot@gmail.com>
2026-08-18 11:37:49 +0530

    Add files via upload

---
+++ src/example_config.py
@@ -0,0 +1,31 @@
+# for tortoise-orm
+
+TORTOISE = {}
+
+
+POSTGRESQL = {}
+
+EXTENSIONS = ()
+
+DISCORD_TOKEN = ""
+
+COLOR = 0x00FFB3
+
+FOOTER = "quo is lub!"
+
+PREFIX = "q"
+
+SERVER_LINK = ""
+
+BOT_INVITE = ""
+
+WEBSITE = ""
+
+REPOSITORY = ""
+
+DEVS = ()
+
+# LOGS
+SHARD_LOG = ""
+ERROR_LOG = ""
+PUBLIC_LOG = ""
