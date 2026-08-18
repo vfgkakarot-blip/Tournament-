3d43354eb1d0204a721412c1d86a872862267d87
vfgkakarot-blip <vfgkakarot@gmail.com>
2026-08-18 13:04:06 +0530

    Add files via upload

---
+++ src/models/helpers/__init__.py
@@ -0,0 +1,3 @@
+from .cfields import *  # noqa: F401, F403
+from .functions import *  # noqa: F401, F403
+from .validators import *  # noqa: F401, F403
---
+++ src/models/helpers/cfields.py
@@ -0,0 +1,16 @@
+from typing import Any
+
+from tortoise.fields.base import Field
+
+
+class ArrayField(Field, list):
+    def __init__(self, field: Field, **kwargs) -> None:
+        super().__init__(**kwargs)
+        self.sub_field = field
+        self.SQL_TYPE = "%s[]" % field.SQL_TYPE
+
+    def to_python_value(self, value: Any) -> Any:
+        return list(map(self.sub_field.to_python_value, value))
+
+    def to_db_value(self, value: Any, instance: Any) -> Any:
+        return [self.sub_field.to_db_value(val, instance) for val in value]
